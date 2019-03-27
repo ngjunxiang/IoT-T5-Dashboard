53b1399b7ea8def074bc703212c0f296256db8a4
Derrick Lee <derrickleemy@gmail.com>
2019-03-27 16:39:03 +0800

    Removed unused HomeController and registration routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,17 @@
 |
  */
 
-Auth::routes();
-Route::group(['middleware' => ['roles', 'auth'], 'roles' => ['tenant']], function () {
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::group(['middleware' => ['roles', 'auth'], 'roles' => ['tenant', 'manager']], function () {
     Route::get('/', 'DashboardController@index')->name('index');
 });
