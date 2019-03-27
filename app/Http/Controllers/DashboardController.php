7d6fde293f77eb388b96a6fc487877691b10e03c
Derrick Lee <derrickleemy@gmail.com>
2019-03-27 10:57:49 +0800

    Added Guzzle and API call to retrieve all listings

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $liveImages = $this->getLiveImages();
+        return view('welcome');
+    }
+
+    public function getLiveImages()
+    {
+        $client = new Client();
+        $response = $client->get(env('API_HOST') . '/api/liveimage/');
+        if ($response->getStatusCode() === 200) {
+            $decodedResponse = json_decode($response->getBody()->getContents(), true);
+            if ($decodedResponse['success'] && $decodedResponse['status'] = 200) {
+                return $decodedResponse['images'];
+            }
+        }
+
+        return null;
+    }
+}
