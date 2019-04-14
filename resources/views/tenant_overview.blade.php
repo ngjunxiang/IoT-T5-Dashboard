7e2bd95dd385e09f53a6bdebeb812017a5418729
krli2016 <kangrui.li.2016@smu.edu.sg>
2019-04-14 22:18:43 +0800

    Update tenant_overview.blade.php
    Update db when seats is requested

--- resources/views/tenant_overview.blade.php
+++ resources/views/tenant_overview.blade.php
@@ -97,6 +97,19 @@ Plotly.newPlot('myDiv', data, layout, { responsive: true });
         $(this).text("REQUESTED");
         $(this).addClass("btn-danger");
         $(this).removeClass("btn-success");
+        $.ajax ({
+        type: "POST",
+        url: "http://3.1.241.179/api/tenants/request",
+        datatype: "application/json",
+        contentType:"application/json",
+        data: JSON.stringify({ 
+            'requested': true,
+            'email':'tenant@hubquarters.com'
+            }),
+        success: function (e) {
+            console.log(e);
+        }
+        });
     });
 
 
